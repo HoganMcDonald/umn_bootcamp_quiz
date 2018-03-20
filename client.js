1e21a5a57137f255b57e4fa52285c434e7b571b4
Hogan McDonald <hpeter.mcdonald@icloud.com>
2018-03-20 17:44:55 -0500

    code working

---
+++ client.js
@@ -0,0 +1,81 @@
+"use strict";
+
+// questions pulled from w3 schools
+const questions = [
+  {
+    question: 'Inside which HTML element do we put the JavaScript?',
+    options: ['javascript', 'script', 'scripting', 'js'],
+    correctAnswer: 'script',
+    userGuess: ''
+  },
+  {
+    question: 'What is the correct syntax for referring to an external script called "xxx.js"?',
+    options: ['script src= xxx.js ', 'script name= xxx.js ', 'script href= xxx.js '],
+    correctAnswer: 'script src= xxx.js ',
+    userGuess: ''
+  },
+  {
+    question: 'Inside which HTML element do we put the JavaScript?',
+    options: ['javascript', 'script', 'scripting', 'js'],
+    correctAnswer: 'script',
+    userGuess: ''
+  },
+  {
+    question: 'What is the correct syntax for referring to an external script called "xxx.js"?',
+    options: ['script src= xxx.js ', 'script name= xxx.js ', 'script href= xxx.js '],
+    correctAnswer: 'script src= xxx.js ',
+    userGuess: ''
+  }
+]
+
+
+// evaluates how many questions were correct
+function evaluate(questionArray) {
+
+  // javascript 30 - Wes Bos
+
+  return questionArray
+    .map(function(question) {
+      return Number(question.correctAnswer === question.userGuess)
+    })
+    .reduce(function(accumulator, currentValue) {
+      return accumulator + currentValue
+    })
+
+} // evaluate()
+
+
+// handles click event for a guess
+$('#questions').on('click', '.radio-input', function() {
+  const answer = $(this);
+  questions[answer.data('key')].userGuess = answer.val();
+}); // radio-input handler
+
+
+
+
+
+// don't worry about code below as much, but feel free to check it out
+
+// renders questions to the page --> not a great solution...
+function render(questionArray) {
+  for (var i = 0; i < questionArray.length; i++) {
+    const q = questionArray[i];
+    let $card = $('<div class="card">');
+    $card.append(`<div class="card-header"><h4 class="title is-4">${q.question}</h4></div>`);
+    let control = q.options.map( option => `<label class="radio"><input class="radio-input" type="radio" name="answer" value="${option}" data-key="${i}">${option}</label>`).join('');
+    $card.append(`<div class="card-content"><form id="question-${i}"><div class="control">${control}</div></form></div>`)
+    $('#questions').append($card);
+  }
+} // render
+
+// init stuff
+render(questions);
+$('#results').hide();
+// handle submit
+$('#submit').on('click', function() {
+  $('#quiz').hide();
+  let $results = $('#results');
+  $results.append(`<h1 class="title is-1">Score: ${evaluate(questions)}/${questions.length}</h1>`)
+  $results.show();
+});
